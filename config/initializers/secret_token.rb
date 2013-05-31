56bc6f4d628afa5ad274a9ddb8b552dbe03d05b5
Jade O'Neill <jade@opencog.org>
2013-05-31 15:10:35 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e746f44a14823457eee74429c52e96c55a098749f0db04b832b2bcfa760ccdfd8aeb704b4248876e4e68f46710860206a86e3c8a8730586fa6487b3eedcce3ec'
